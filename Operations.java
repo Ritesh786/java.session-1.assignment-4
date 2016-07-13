e9eb5473cbf70aa60340556a7bf8d81371d7841c
Ritesh786 <jharitesh6@gmail.com>
2016-07-13 15:34:14 +0530

    session 1, assignment 4
    session 1, assignment 4

---
+++ Operations.java
@@ -0,0 +1,11 @@
+
+public class Operations {
+
+	public static void main(String[] args) {
+		int x=5,y=10;
+		System.out.println("Value of x+y*2 = "+(x+y*2));
+        System.out.println("Value  of x-y+2 = "+(x-y+2));
+	   System.out.println("Value of (x+y)*2 = "+((x+y)*2));
+	   System.out.println("Value of y%x = "+(y%x));
+
+}}
